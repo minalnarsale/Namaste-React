f14ac171c758fefde3fe903949d4e7e00acdcbe8
minalnarsale <minalnarsale@gmail.com>
2025-06-25 11:14:45 +0530

    [React]: project basics

--- App.js
+++ App.js
@@ -1,5 +1,30 @@
-const heading = React.createElement("h1", {id: "heading"}, "rendering Hello World using React!");  //this is not an h1 tag. it is an react element/object, check by printing it's value
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement(    //this is not an h1 tag. it is an react element/object, check by printing it's value
+    "h1",
+    {id: "heading"},
+    "rendering Hello World using React!"
+);
 console.log('heading : ', heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));                                  // it is also element/object
-console.log('root : ', root);                                       
-root.render(heading);
+console.log('root : ', root); 
+// root.render(heading);  
+
+const TitleElement = ( <h1 className="title1">Title/TitleElement: Namaste React</h1>
+)
+const TitleComponent = () => ( <h1 className="title1">Title/TitleComponent: Namaste React</h1>
+)
+
+const number = 1000;
+const JsxHeading = () => (      //this is React component
+    <div>
+        {TitleElement}
+        <TitleComponent />
+        <h1>{number}</h1>
+        <h1 className="jsxHeading">Heading: This is component composition</h1>
+    </div>
+)
+
+// const JsxHeading = () => <h1 className="jsxHeading">rendering Hello World using React JSX!</h1>
+root.render(<JsxHeading />);
