c6f6304d8b189e13d9215f265526ac8b618fe495
minalnarsale <minalnarsale@gmail.com>
2025-06-30 13:32:51 +0530

    episode4: dynamic restaw cards added

--- App.js
+++ App.js
@@ -1,30 +1,67 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(    //this is not an h1 tag. it is an react element/object, check by printing it's value
-    "h1",
-    {id: "heading"},
-    "rendering Hello World using React!"
-);
-console.log('heading : ', heading);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img 
+                    className="logo" 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB2rn_FyXKvp_W8IfgwjeJiaCqyLo1cg0XFGSm00b-SzUhcnWz7U6NPE0&s" 
+                /> 
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About US</li>
+                    <li>Contact US</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestauCards = (props) => {
+    console.log('props : ', props);
+    return (
+        <div className="restau-card">
+            <img 
+                className="restau-logo"
+                alt="restau-logo" 
+                src="https://cdn.britannica.com/35/225835-050-A5CC289A/Indian-one-pot-meal-for-party.jpg"
+            />
+            <h3>{props.RestauName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 mins</h4>
+        </div>    
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search-bar-container">
+                <input className="search-bar" type="text"/>
+                <button className="search-button">Search</button>
+            </div>
+            <div className="restau-cards-container">
+                <RestauCards RestauName='Meghna Foods' cuisine='Biryani, North Indian, Asian'/>
+                <RestauCards RestauName='KFC' cuisine='Burgers, Fast Food'/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));                                  // it is also element/object
 console.log('root : ', root); 
-// root.render(heading);  
-
-const TitleElement = ( <h1 className="title1">Title/TitleElement: Namaste React</h1>
-)
-const TitleComponent = () => ( <h1 className="title1">Title/TitleComponent: Namaste React</h1>
-)
-
-const number = 1000;
-const JsxHeading = () => (      //this is React component
-    <div>
-        {TitleElement}
-        <TitleComponent />
-        <h1>{number}</h1>
-        <h1 className="jsxHeading">Heading: This is component composition</h1>
-    </div>
-)
-
-// const JsxHeading = () => <h1 className="jsxHeading">rendering Hello World using React JSX!</h1>
-root.render(<JsxHeading />);
+root.render(<AppLayout />);
